c21b6635ee558a886b4488bdddb5bf0df9562d61
WEBSTERAD\wustudent <dts@webster.edu>
2022-02-19 19:48:24 -0600

    noises

--- GAMEJAMLOVEYOURPET/Assets/Script_Nick/TourneyManager.cs
+++ GAMEJAMLOVEYOURPET/Assets/Script_Nick/TourneyManager.cs
@@ -1,8 +1,32 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.SceneManagement;
 public class TourneyManager : MonoBehaviour
 {
+    public int tourneypoints;
+    public static int loadthis;
+    void Start()
+    {
+        loadthis = Random.Range(0, 3);
+        tourneypoints =0;
+        if (loadthis == 0) 
+        {
+            SceneManager.LoadScene("FireScene");
+        }
+        if (loadthis == 1)
+        {
+            SceneManager.LoadScene("Swimming Scene");
+        }
+        if (loadthis == 2)
+        {
+            SceneManager.LoadScene("flappyfriend");
+        }
+        if (loadthis == 3)
+        {
+            SceneManager.LoadScene("beansgame");
+        }
+        
+    }
     
 }
